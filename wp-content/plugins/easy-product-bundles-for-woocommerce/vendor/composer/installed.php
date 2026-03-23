b7e2d5512d88ac8b953b7644949a4317844846a2
Noriks <noriks-bot@noriks.com>
2026-03-23 17:34:05 +0100

    init: tema + plugini

---
+++ wp-content/plugins/easy-product-bundles-for-woocommerce/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'asanaplugins/easy-product-bundles-woocommerce',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7a430ef5ab0a1e33b15e9024afa891f16514110a',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'asanaplugins/easy-product-bundles-woocommerce' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7a430ef5ab0a1e33b15e9024afa891f16514110a',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
